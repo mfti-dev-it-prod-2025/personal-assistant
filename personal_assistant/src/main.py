159e897db1eb873d600330ebde52bbd6e589bca6
AndreyQuantum <zelentsov1254@gmail.com>
2025-11-11 13:59:59 +0100

    init base endpoints

---
+++ personal_assistant/src/main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+
+from api.v1.misc import router as misc_router
+from personal_assistant.src.configs.app import settings
+
+app = FastAPI(
+    title="Personal assistant",
+
+)
+
+
+app.include_router(misc_router, prefix="/api/v1/misc", tags=["misc"])
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.app.app_host,
+        port=settings.app.app_port,
+    )
