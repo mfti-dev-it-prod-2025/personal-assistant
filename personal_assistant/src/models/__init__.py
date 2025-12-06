442e1c3376de6fa8e2a619996f0497142ace3b5a
kusdmi <kusdmi@yandex.ru>
2025-12-06 18:27:58 +0300

    update __init__.py

--- personal_assistant/src/models/__init__.py
+++ personal_assistant/src/models/__init__.py
@@ -1,1 +1,5 @@
-from .user import UserTable
+from .user import UserTable, UserBase, UserRole
+from .todo import Task
+from .base import BaseTable
+
+__all__ = ["UserTable", "UserBase", "UserRole", "Task", "BaseTable"]
