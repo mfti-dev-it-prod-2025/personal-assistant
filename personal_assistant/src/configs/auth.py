3860fd0daf4d254e4e203d4466bdc32cfd00c0b9
kusdmi <kusdmi@yandex.ru>
2025-12-06 21:56:05 +0300

    updating user rights

--- personal_assistant/src/configs/auth.py
+++ personal_assistant/src/configs/auth.py
@@ -4,10 +4,12 @@ SCOPES: dict[str, str] = {
     "users:read": "Чтение списка пользователей и их данных",
     "users:write": "Создание и изменение пользователей",
     "me:read": "Чтение собственных данных пользователя",
+    "tasks:read": "Чтение задач",  # ДОБАВЛЕНО
+    "tasks:write": "Создание и изменение задач",  # ДОБАВЛЕНО
 }
 ROLES_TO_SCOPES = {
     "administrator": list(SCOPES.keys()),
-    "user": ["me:read"],
+    "user": ["me:read", "tasks:read", "tasks:write"],  # ДОБАВЛЕНЫ SCOPES ДЛЯ ЗАДАЧ
 }
 oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl="/api/v1/auth/token",
